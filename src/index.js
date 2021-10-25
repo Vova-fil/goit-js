cb4c500a6a783ac4713b0e7390c9b652ea228256
Vova-fil <vfil.zp@gmail.com>
2021-10-26 00:25:23 +0300

    модуль 01,2

--- src/index.js
+++ src/index.js
@@ -1,43 +1,120 @@
-// ======= hoisting ======
-// let a = 1;
-// const b = 2;
+console.log('Module 1.2');
 
-// console.log('a:', a);
-// console.log('b:', b);
-// console.log('c:', c);
+// ===== break =====
+// for (let i = 0; i < 100; i += 1) {
+//   console.log(i);
 
-// var c = 3;
-
-// console.log('c:', c);
-
-// ======= область видимости =======
-// {
-//   var x = 111;
-//   console.log('x:', x);
+//   if (i === 5) {
+//     console.log(i);
+//     break;
+//   }
 // }
 
-// console.log('x:', x);
+// ===== continue =====
+// for (let i = 0; i < 100; i++) {
+//   if (i % 5 !== 0) {
+//     continue;
+//   }
 
-// {
-//   let y = 222;
-//   console.log('y:', y);
+//   console.log(i);
 // }
 
-// console.log('y:', y);
-
-// ======= цикл =======
-// for (let i = 0; i < 10; i += 1) {
-//   console.log('hello:', i);
+// ===== ternary =====
+// if (true) {
+//   console.log(1);
+// } else {
+//   console.log(2);
 // }
 
-// console.log('last i:', i);
+// console.log(x > 5 ? 1 : 0);
 
-// ===== const VS CONST =====
-// const PI = 3.14;
+// ===== slice =====
+//           0123456789
+// const str = 'I love JS!';
+// const subStr1 = str.slice(7, 9);
+// const subStr2 = str.substring(7, 9);
+// const subStr3 = str.substr(7, 2);
 
-// const a = 5;
-// const b = 10;
-// const c = a + b;
+// console.log(subStr1); // slice
+// console.log(subStr2); // substring
+// console.log(subStr3); // substr
 
-// console.log('c:', c);
-// console.log('PI:', PI);
+// =====================
+// Функция checkForSpam(message) принимает строку (параметр message),
+// проверяет её на содержание запрещенных слов spam и sale,
+// и возвращает результат проверки. Слова в строке параметра message
+// могут быть в произвольном регистре, например SPAM или sAlE.
+
+// Если нашли запрещенное слово (spam или sale)
+// то функция возвращает буль true.
+// Если в строке нет запрещенных слов,
+// функция возвращает буль false.
+
+// const checkForSpam = (message) => {
+//   const words = ['spam', 'sale', 'taxi', 'robot']; // string
+//   const str = message.toLowerCase();
+//   let hasSpam = false;
+
+//   for (let i = 0; i < words.length; i += 1) {
+//     const word = words[i];
+
+//     if (str.includes(word)) {
+//       hasSpam = true;
+//       break;
+//     }
+//   }
+
+//   return hasSpam; // true | false
+// };
+
+// console.log(checkForSpam('lorem ipsum')); // false
+// console.log(checkForSpam('lorem sPaM ipsum')); // true
+// console.log(checkForSpam('lorem sALE ipsum')); // true
+// console.log(checkForSpam('lorem robot ipsum')); // true
+// console.log(checkForSpam('lorem taxi ipsum')); // true
+
+// =========== login and password ===========
+// const checkLogin = (userLogin) => {
+//   const logins = ['admin', 'root', 'user']; // string
+//   // let hasLogin = false;
+
+//   // for (const login of logins) {
+//   //   if (userLogin === login) {
+//   //     hasLogin = true;
+//   //     break;
+//   //   }
+//   // }
+
+//   // return hasLogin; // true | false
+//   return logins.includes(userLogin);
+// };
+
+// console.log(checkLogin('dfghdfh')); // false
+// console.log(checkLogin('ert')); // false
+// console.log(checkLogin('admin')); // true
+// console.log(checkLogin('user')); // true
+
+// ===== kahoot 1 =====
+// 0 || 'Mango'
+// console.log(0 || 'Mango');
+// if (0 || 'Mango') {
+//   console.log('hurray');
+// }
+
+// 0 || false
+// console.log(false || 0);
+// if (false || 0) {
+//   console.log('hurray');
+// }
+
+// 'Poly' || 'Mango'
+// console.log('Poly' || 'Mango');
+
+// const userInput = prompt('enter name');
+// console.log('Hello:', userInput || 'Nemo');
+
+// ======= kahoot =======
+// [0]
+// if ([0]) {
+//   console.log('hurray');
+// }
